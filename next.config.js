f5595fa66a42206090d4c6ba69f8585c1f73f1b9
Joe Thomas <thjo0505@gmail.com>
2021-08-07 23:21:34 -0500

    added mdx for blogging

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
 }
+// next.config.js
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+})
+
